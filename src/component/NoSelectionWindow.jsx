b846f50c76079be9f3aa6d48ac52d2b3be6b60b3
Muthana-Alsaigh <mothanasyagha@gmail.com>
2025-10-19 21:56:38 +0300

    new

--- src/component/NoSelectionWindow.jsx
+++ src/component/NoSelectionWindow.jsx
@@ -2,15 +2,15 @@ import image from "../assets/no-projects.png"
 import BrounButton from "./buttons/brounButton"
 
 export default function NoSelectionWindow({onAdding}) {
-  const marginNo = "mt-5 font-bold"
+  const sharedStyles = "mt-5 font-bold"
 
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <img src={image} alt="No projects image" className="w-20 h-20 object-cover"/>
-      <h2 className={marginNo+" text-stone-600 text-2xl"}>No Project Selected</h2>
-      <h3 className={marginNo+" text-stone-400 text-lg"}>Select a project or get started with a new one</h3>
+      <h2 className={sharedStyles+" text-stone-600 text-2xl"}>No Project Selected</h2>
+      <h3 className={sharedStyles+" text-stone-400 text-lg"}>Select a project or get started with a new one</h3>
       <BrounButton 
-        textColor={marginNo+" text-stone-400 font-medium"}
+        textColor={sharedStyles+" text-stone-400 font-medium"}
         onClick={onAdding}
       >
         Create new project
